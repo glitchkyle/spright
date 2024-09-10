7906de006407307686476dfcf3cfcaca2ea561fc
klast <klast@localhost>
2024-09-10 16:01:02 -0600

    [chore]: upgrade knative from v0.22.0 to v1.15.2

--- sigcomm-experiment/env-setup/300-knative_install.sh
+++ sigcomm-experiment/env-setup/300-knative_install.sh
@@ -7,14 +7,25 @@ then
 	exit 1
 fi
 
-echo 'Install Knative Serving v0.22.0'
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.0/serving-crds.yaml
-kubectl apply -f ./sigcomm-experiment/env-setup/serving-core.yaml
+echo 'Install Knative Serving v1.15.2'
 
-echo 'Install Istio v0.22.0'
-kubectl apply -f ./sigcomm-experiment/env-setup/istio.yaml # Install a properly configured Istio
-kubectl apply -f https://github.com/knative/net-istio/releases/download/v0.22.0/net-istio.yaml # Install the Knative Istio controller
-kubectl --namespace istio-system get service istio-ingressgateway # Fetch the External IP or CNAME
+# Install the required custom resources by running the command:
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.15.2/serving-crds.yaml
+# Install the core components of Knative Serving by running the command:
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.15.2/serving-core.yaml
+
+echo 'Install Kourier v1.15.2'
+# Install the Knative Kourier controller by running the command:
+kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.15.1/kourier.yaml
+
+# Configure Knative Serving to use Kourier by default by running the command:
+kubectl patch configmap/config-network \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
+
+# Fetch the External IP address or CNAME by running the command:
+kubectl --namespace kourier-system get service kourier
 
 # echo 'Install the required CRDs and the core components of Eventing'
 # kubectl apply -f https://github.com/knative/eventing/releases/download/v0.22.0/eventing-crds.yaml
