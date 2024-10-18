0d125540f2b09d23d8b43200f4a2444ec74a6d4f
klast <klast@localhost>
2024-10-17 20:40:51 -0500

    [fix]: restructure Docker setup

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-CONTAINER_NAME="spright_container"
+# Define container names and paths
+CONTAINER_NAME=$2
+IMAGE_NAME="spright_image"
 
-# Start the Docker container
-docker run --name $CONTAINER_NAME \
-  --privileged \  # Required for eBPF and NIC device access
-  --net=host \    # Host network for low-latency networking
-  --device=/dev/net/tun \  # NIC access for eBPF programs
-  --device=/dev/uio0 \     # DPDK NIC device mapping
-  -v /dev/hugepages:/dev/hugepages \  # Map host hugepage shared memory into container
-  -v $MYMOUNT:/mydata \            # Map the entire /mydata directory to container's /mydata
-  -it $1 \   # Container image name passed as argument
-  /bin/bash  # Start the container interactively
+if [ "$1" = "shm_mgr" ]; then
+    COMMAND="/mydata/spright/bin/shm_mgr_rte_ring /mydata/spright/configs/shm_mgr_config.cfg"
+elif [ "$1" = "gateway" ]; then
+    COMMAND="/mydata/spright/bin/gateway_rte_ring"
+elif [ "$1" = "nf" ]; then
+    COMMAND="/mydata/spright/bin/nf_rte_ring $3"  # Pass NF_ID as $3
+else
+    echo "Invalid component: $1"
+    exit 1
+fi
+
+# Start the Docker container with the appropriate command
+sudo docker run \
+  --detach=true \
+  --privileged \
+  --name $CONTAINER_NAME \
+  --network bridge \
+  --device=/dev/net/tun \
+  --device=/dev/uio0 \
+  --volume /dev/hugepages:/dev/hugepages \
+  $IMAGE_NAME $COMMAND
