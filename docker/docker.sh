51bdf8e171ad0e546e645626f76ba20ba3e8f65b
klast <klast@localhost>
2024-10-27 20:55:11 -0500

    change compilation to mounts

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-# Define container names and paths
+$IMAGE_NAME=$1
 CONTAINER_NAME=$2
-IMAGE_NAME="spright_image"
 
-if [ "$1" = "shm_mgr" ]; then
-    COMMAND="/mydata/spright/bin/shm_mgr_rte_ring /mydata/spright/configs/shm_mgr_config.cfg"
-elif [ "$1" = "gateway" ]; then
-    COMMAND="/mydata/spright/bin/gateway_rte_ring"
-elif [ "$1" = "nf" ]; then
-    COMMAND="/mydata/spright/bin/nf_rte_ring $3"  # Pass NF_ID as $3
-else
-    echo "Invalid component: $1"
-    exit 1
-fi
-
-# Start the Docker container with the appropriate command
-sudo docker run \
-  --detach=true \
-  --privileged \
+sudo docker run -d \
   --name $CONTAINER_NAME \
+  --privileged \
   --network bridge \
   --device=/dev/net/tun \
   --device=/dev/uio0 \
-  --volume /dev/hugepages:/dev/hugepages \
-  $IMAGE_NAME $COMMAND
+  -v /dev/hugepages:/dev/hugepages \
+  -v /var/run/dpdk:/var/run/dpdk \
+  -v /lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu \
+  -v /usr/local/lib/x86_64-linux-gnu:/usr/local/lib/x86_64-linux-gnu \
+  -v /usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu \
+  -v /usr/lib64:/usr/lib64 \
+  -v /mydata/spright:/mydata/spright \
+  $IMAGE_NAME
